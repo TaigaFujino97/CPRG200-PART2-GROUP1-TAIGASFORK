82444282f252d08cb0e130a605f42ad1f355c3e6
FaurnsGoldberg <chessall007@gmail.com>
2024-02-14 12:51:34 -0700

    Revert "Revert "Merge pull request #16 from OwenHuot/main""
    This reverts commit 65ce9bd9a2fea73edf65d2ca2197e5583c504db3.

---
+++ TravelExpertsApp/TravelExpertsForm/AgentsForm.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace TravelExpertsForm
+{
+    partial class AgentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            groupBox7 = new GroupBox();
+            textBox7 = new TextBox();
+            groupBox6 = new GroupBox();
+            textBox6 = new TextBox();
+            groupBox5 = new GroupBox();
+            textBox5 = new TextBox();
+            groupBox4 = new GroupBox();
+            textBox4 = new TextBox();
+            groupBox3 = new GroupBox();
+            textBox3 = new TextBox();
+            groupBox2 = new GroupBox();
+            textBox2 = new TextBox();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            groupBox7.SuspendLayout();
+            groupBox6.SuspendLayout();
+            groupBox5.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(groupBox7, 0, 6);
+            tableLayoutPanel1.Controls.Add(groupBox6, 0, 5);
+            tableLayoutPanel1.Controls.Add(groupBox5, 0, 4);
+            tableLayoutPanel1.Controls.Add(groupBox4, 0, 3);
+            tableLayoutPanel1.Controls.Add(groupBox3, 0, 2);
+            tableLayoutPanel1.Controls.Add(groupBox2, 0, 1);
+            tableLayoutPanel1.Controls.Add(groupBox1, 0, 0);
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 14.2857141F));
+            tableLayoutPanel1.Size = new Size(291, 504);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox7
+            // 
+            groupBox7.Controls.Add(textBox7);
+            groupBox7.Location = new Point(3, 435);
+            groupBox7.Name = "groupBox7";
+            groupBox7.Size = new Size(285, 63);
+            groupBox7.TabIndex = 7;
+            groupBox7.TabStop = false;
+            groupBox7.Text = "groupBox7";
+            // 
+            // textBox7
+            // 
+            textBox7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox7.Location = new Point(6, 26);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(273, 27);
+            textBox7.TabIndex = 0;
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(textBox6);
+            groupBox6.Location = new Point(3, 363);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(285, 63);
+            groupBox6.TabIndex = 6;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "groupBox6";
+            // 
+            // textBox6
+            // 
+            textBox6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox6.Location = new Point(6, 26);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(273, 27);
+            textBox6.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(textBox5);
+            groupBox5.Location = new Point(3, 291);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(285, 63);
+            groupBox5.TabIndex = 5;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "groupBox5";
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox5.Location = new Point(6, 26);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(273, 27);
+            textBox5.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(textBox4);
+            groupBox4.Location = new Point(3, 219);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(285, 63);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "groupBox4";
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox4.Location = new Point(6, 26);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(273, 27);
+            textBox4.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Location = new Point(3, 147);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(285, 63);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(6, 26);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(273, 27);
+            textBox3.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Location = new Point(3, 75);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(285, 63);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(6, 26);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(273, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Location = new Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(285, 63);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(6, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(273, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Courier New", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 17;
+            listBox1.Location = new Point(309, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(902, 412);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel2.Controls.Add(radioButton3, 2, 0);
+            tableLayoutPanel2.Controls.Add(radioButton2, 1, 0);
+            tableLayoutPanel2.Controls.Add(radioButton1, 0, 0);
+            tableLayoutPanel2.Location = new Point(309, 440);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(564, 65);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // radioButton3
+            // 
+            radioButton3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            radioButton3.AutoSize = true;
+            radioButton3.CheckAlign = ContentAlignment.BottomCenter;
+            radioButton3.Location = new Point(379, 3);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(182, 59);
+            radioButton3.TabIndex = 5;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "MODIFY";
+            radioButton3.TextAlign = ContentAlignment.MiddleCenter;
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            radioButton2.AutoSize = true;
+            radioButton2.CheckAlign = ContentAlignment.BottomCenter;
+            radioButton2.Location = new Point(191, 3);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(182, 59);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "REMOVE";
+            radioButton2.TextAlign = ContentAlignment.MiddleCenter;
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            radioButton1.AutoSize = true;
+            radioButton1.CheckAlign = ContentAlignment.BottomCenter;
+            radioButton1.Location = new Point(3, 3);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(182, 59);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "ADD";
+            radioButton1.TextAlign = ContentAlignment.MiddleCenter;
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(879, 444);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 61);
+            button1.TabIndex = 3;
+            button1.Text = "SUBMIT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1048, 444);
+            button2.Name = "button2";
+            button2.Size = new Size(163, 61);
+            button2.TabIndex = 4;
+            button2.Text = "RETURN";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // AgentsForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1223, 517);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(tableLayoutPanel2);
+            Controls.Add(listBox1);
+            Controls.Add(tableLayoutPanel1);
+            MaximumSize = new Size(1241, 564);
+            MinimumSize = new Size(1241, 564);
+            Name = "AgentsForm";
+            Text = "AgentsForm";
+            tableLayoutPanel1.ResumeLayout(false);
+            groupBox7.ResumeLayout(false);
+            groupBox7.PerformLayout();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private GroupBox groupBox7;
+        private TextBox textBox7;
+        private GroupBox groupBox6;
+        private TextBox textBox6;
+        private GroupBox groupBox5;
+        private TextBox textBox5;
+        private GroupBox groupBox4;
+        private TextBox textBox4;
+        private GroupBox groupBox3;
+        private TextBox textBox3;
+        private GroupBox groupBox2;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private Button button1;
+        private Button button2;
+    }
+}
