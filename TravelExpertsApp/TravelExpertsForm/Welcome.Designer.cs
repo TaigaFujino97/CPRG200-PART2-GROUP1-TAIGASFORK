397dd28377fd8503b40c3f98ac2b019a8d821225
FaurnsGoldberg <chessall007@gmail.com>
2024-02-14 12:54:54 -0700

    Revert "Merge branch 'main' of https://github.com/FaurnsGoldberg/CPRG200-PART2-GROUP1"
    This reverts commit 608ef9025efccfb83a0fb65134f38b56c6d237e9, reversing
    changes made to 82444282f252d08cb0e130a605f42ad1f355c3e6.

--- TravelExpertsApp/TravelExpertsForm/Welcome.Designer.cs
+++ TravelExpertsApp/TravelExpertsForm/Welcome.Designer.cs
@@ -28,15 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
             btnPackages = new Button();
             btnSuppliers = new Button();
             btnPackage = new Button();
             btnAgents = new Button();
             tableLayoutPanel1 = new TableLayoutPanel();
             tableLayoutPanel1.SuspendLayout();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btnPackages
@@ -119,6 +116,5 @@
         private Button btnPackage;
         private Button btnAgents;
         private TableLayoutPanel tableLayoutPanel1;
-        private PictureBox pictureBox1;
     }
 }
